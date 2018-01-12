ecf844f5ba56835e600f389a3d22d1af2b6b480e
Aleks Vino <aleks.vino@gmail.com>
2018-01-12 02:07:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-var name = "susan"
+var name = "Joe"
 var height = "74"
-if (name === "susan") {console.log ("correct") }
-else { console.log("wrong") }
 
 // Don't worry about this
 module.exports = { name, height
